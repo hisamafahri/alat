8b4bec0e487f4b9bd2c9e215a553a97bf2ad6f92
Hisam Fahri <me@hisamafahri.com>
2022-06-03 09:35:32 +0700

    feat(tailwind): configure tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,23 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import type { NextPage } from "next";
+import Head from "next/head";
 
 const Home: NextPage = () => {
-  return (
-    <div>
-      <Head>
-        <title>alat - Tools for every developer</title>
-        <meta name="description" content="Helping every developer with their tasks" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <div>
+            <Head>
+                <title>alat - Tools for every developer</title>
+                <meta
+                    name="description"
+                    content="Helping every developer with their tasks"
+                />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-      <main></main>
-    </div>
-  )
-}
+            <main>
+                <h1 className="italic">Hello world!</h1>
+            </main>
+        </div>
+    );
+};
 
-export default Home
+export default Home;
